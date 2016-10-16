cdee66c8144c785b1f32a117c47b4a9904453b25
宁静致远 <379531702@qq.com>
2016-10-16 10:31:53 +0800

    init

---
+++ src/com/lsy/test/LaunchActivity.java
@@ -0,0 +1,22 @@
+package com.lsy.test;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class LaunchActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+                startActivity(new Intent(LaunchActivity.this,MainActivity.class));
+            }
+        },1000);
+    }
+}
