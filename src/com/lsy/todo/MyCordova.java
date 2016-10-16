a245a1f77f47318ba6603c3f25a0cbaa04a02eaa
宁静致远 <379531702@qq.com>
2016-10-16 16:03:57 +0800

    sharesdk

--- src/com/lsy/todo/MyCordova.java
+++ src/com/lsy/todo/MyCordova.java
@@ -1,22 +1,32 @@
 package com.lsy.todo;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import android.view.View;
+
 import com.lsy.test.R;
 import com.lsy.todo.tools.Utils;
 
-public class MyCordova extends CordovaPlugin{
+public class MyCordova extends CordovaPlugin {
 
 	@Override
 	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-		if(action.endsWith("share")){
+		if (action.endsWith("share")) {
 			String title = this.cordova.getActivity().getString(R.string.app_name);
 			String text = this.cordova.getActivity().getString(R.string.app_desc);
-			String imagePath = "";
-			new Utils().showShare(this.cordova.getActivity(), title, text, imagePath);
+			
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss", Locale.US);
+			String imagePath = "/sdcard/" + sdf.format(new Date()) + ".png";
+			View view = this.cordova.getActivity().getWindow().getDecorView().findViewById(android.R.id.content);
+			Utils.saveScreen(view, imagePath);
+			Utils.showShare(this.cordova.getActivity(), title, text, imagePath);
 		}
 		return super.execute(action, args, callbackContext);
 	}
--- src/com/lsy/todo/tools/Utils.java
+++ src/com/lsy/todo/tools/Utils.java
@@ -1,21 +1,42 @@
 package com.lsy.todo.tools;
 
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.view.View;
 import cn.sharesdk.framework.ShareSDK;
 
 import com.sharesdk.onekeyshare.OnekeyShare;
 
 public class Utils {
 
-	public void showShare(Context context,String title,String text,String imagePath) {
+	public static void showShare(Context context, String title, String text, String imagePath) {
 		ShareSDK.initSDK(context);
 		OnekeyShare oks = new OnekeyShare();
 		oks.disableSSOWhenAuthorize();
 		oks.setTitle(title);
 		oks.setText(text);
 		oks.setImagePath(imagePath);
-//		 oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
 		oks.show(context);
 	}
 
+	public static void saveScreen(View view,String imagePath) {
+		view.setDrawingCacheEnabled(true);
+		view.buildDrawingCache();
+		Bitmap bitmap = view.getDrawingCache();
+		if (bitmap != null) {
+			try {
+				FileOutputStream out = new FileOutputStream(imagePath);
+				bitmap.compress(Bitmap.CompressFormat.PNG, 100, out);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else {
+			
+		}
+	}
 }
