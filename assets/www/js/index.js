f35bd3b262a91fb0ea088ce0f0125ffa0d2a6ff7
宁静致远 <379531702@qq.com>
2016-10-17 21:12:56 +0800

    page

--- assets/www/js/index.js
+++ assets/www/js/index.js
@@ -1,51 +1,37 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+function toJson(arr){
+	var result ="{";
+	result = result+"title:"+arr.title +",";
+	result = result+"typevalue:"+arr.typevalue +",";
+	result = result+"time:"+arr.time +",";
+	result = result+"text:"+arr.text;
+	result = result+"}";
+	return result;
+}
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+function saveData(data){
+	localStorage.setItem('data',data);
+}
 
-        console.log('Received Event: ' + id);
-    }
-};
+function getData(){
+	return localStorage.getItem('data');
+}
 
-app.initialize();
+$('.btn-commit').click(function(){
+	var data = Array();
+	data.title = $('#title').val();
+	data.typevalue = $('input[name="typevalue"]:checked').val();
+	data.time = $('#time').val();
+	data.text = $('#text').val();
+	data = toJson(data);
+	var oldData = getData();
+	if(oldData != null){
+		oldData = oldData.substr(1,oldData.length-2);
+		data = "["+ oldData +","+ data+"]";
+		console.log(data);
+		saveData(data);
+	}else{
+		data = "["+ data +"]";
+		console.log(data);
+		saveData(data);
+	}
+});
